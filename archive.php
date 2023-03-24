382aad7440100f1131b0f4a3c2574102474f0dcf
marvinoka4 <marvinoka4@gmail.com>
2023-03-24 03:16:54 +0000

    update archive.php

--- archive.php
+++ archive.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
 <?php get_template_part( 'template-parts/content', 'page-head'); ?>
 
-<?php get_template_part( 'template-parts/content', 'sidebar'); ?>
+<?php get_template_part( 'template-parts/content', 'archive'); ?>
 
 
 <?php get_footer(); ?>
